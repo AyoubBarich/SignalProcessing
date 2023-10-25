24dd525b4d2c0def9dbf669f2a4cbdb0bcb3dc77
compatarte <alexteiss01@gmail.com>
2023-10-25 09:59:10 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Main.py
    	new file:   affichagewav.py

---
+++ affichagewav.py
@@ -0,0 +1,23 @@
+import cmath as math
+import scipy.io.wavfile as wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = input("quel est le fichier a visualisé :")
+newrate = int(input("espace entre les points :"))
+rate,data = wavfile.read(path)
+time = np.arange(len(data))/float(rate)
+newtime=[]
+newdata=[]
+newmoddata=[]
+i=0
+while(i<len(time)) :
+    newtime.append(time[i])
+    newdata.append(data[i])
+    newmoddata.append(abs(data[i]))
+    i=i+newrate
+print(newdata)
+print(newmoddata)
+plt.plot(newtime,newdata)
+plt.plot(newtime,newmoddata)
+plt.show()
