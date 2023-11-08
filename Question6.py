977553482894f844428705593466f13ead4c8a9f
GervaisAzevedo <108226831+GervaisAzevedo@users.noreply.github.com>
2023-11-08 10:23:26 +0000

    Question6 issue with epsLN Changes to be committed: 	modified:   Main.py 	modified:   Question6.py 	new file:   out/RealOutPutTest400.wav 	renamed:    testFiles/test1000.wav -> out/test1000.wav 	renamed:    testFiles/test400.wav -> out/test400.wav 	renamed:    testFiles/test600.wav -> out/test600.wav

--- Question6.py
+++ Question6.py
@@ -1,39 +1,30 @@
 import numpy as np
-import scipy.io wavfile as wavfile
+import math as math
+import scipy.io.wavfile as wavfile
 
 N = 4096 #cardDecoupage
+fs = 4096 #fréquence
 T = 1    #temps echantilloné
 possible_l = [400,600,1000]
 
 def epsLN(l,n):
-    return math.exp(complex(0,(2*math.pi*l*n)/self.dim))
+    return math.exp(complex(0,(2*math.pi*l*n)/N))
+
+print(epsLN(400, 1520))
 
 for l in possible_l:
-    musicFileName = "test" + l + ".wav"
-    print("musicFileName is :" musicFileName)
+    musicFileName = "Test" + str(l) + ".wav"
+    outPutPathName = "./out/RealOutPut" + musicFileName
+    print("musicFileName is :" + musicFileName)
 
-    vector_eps_l = np.array (epsLN(l,n) for n in range N)
+    # vector_eps_l = np.fromfunction(lambda i,j : epsLN(l,j), (1,N), dtype = complex)
+    # vector_eps_l = np.array([epsLN(l,n) for n in range(N)], dtype = complex)
+    vector_eps_l = np.array([epsLN(l,n) for n in range(N)], dtype = complex)
     vector_eps_l = vector_eps_l.real
-    wavfile.write( "RealOutPut" + musicFileName, fs, y )
 
+    print("vector_eps_l:")
+    for elt in vector_eps_l:
+        print(elt)
 
+    wavfile.write(outPutPathName, fs, vector_eps_l )
 
-
-
-"""
-from scipy.io.wavfile import write
-
-import numpy as np
-
-samplerate = 44100; fs = 100
-
-t = np.linspace(0., 1., samplerate)
-
-amplitude = np.iinfo(np.int16).max
-
-data = amplitude * np.sin(2. * np.pi * fs * t)
-
-write("example.wav", samplerate, data.astype(np.int16))
-
-"""
-        
