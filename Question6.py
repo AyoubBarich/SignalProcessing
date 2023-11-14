de0c77c8b948c1c3c750091e29b70406e3b8b184
GervaisAzevedo <108226831+GervaisAzevedo@users.noreply.github.com>
2023-11-14 18:03:48 +0000

    modified:   Question6.py 	deleted:    y1.wav

--- Question6.py
+++ Question6.py
@@ -1,6 +1,7 @@
 import cmath as cmath
 import scipy.io.wavfile as wavfile
 import numpy.fft as npft
+import numpy as np
 
 N = 4096 #cardDecoupage
 fs = 4096 #fréquence
@@ -10,13 +11,11 @@ possible_l = [400,600,1000]
 def epsLN(l,n):
     return cmath.exp(complex(0,(2*cmath.pi*l*n)/N))
 
-print(epsLN(400, 1520))
 
 for l in possible_l:
     musicFileName = "Test" + str(l) + ".wav"
     outPutRealPathName = "./out/RealOutPut" + musicFileName
     outPutRealHatPathName = "./out/RealHatOutPut" + musicFileName
-    print("musicFileName is :" + musicFileName)
 
     vector_eps_l = np.array([epsLN(l,n) for n in range(N)])
     vector_eps_l_real = vector_eps_l.real
@@ -24,9 +23,6 @@ for l in possible_l:
     vector_eps_l_hat = npft.ifft(vector_eps_l)
     vector_eps_l_hat_real = vector_eps_l.real
 
-    print("vector_eps_l:")
-    for elt in vector_eps_l:
-        print(elt)
-
-    wavfile.write(outPutPathName, fs, vector_eps_l )
+    wavfile.write(outPutRealPathName, fs, vector_eps_l_real )
+    wavfile.write(outPutRealHatPathName, fs, vector_eps_l_hat_real )
 
